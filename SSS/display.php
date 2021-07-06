82cb6913e55f5c28c83438f7b91975bcf09f2bce
pamran <ranjitpamas@gmail.com>
2021-07-06 09:08:54 -0500

    update file
    having some blockers and then fixing it

--- SSS/display.php
+++ SSS/display.php
@@ -1,9 +1,10 @@
 <?php
-include 'connect.php'; 
+include 'connect.php';
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
   <meta charset="UTF-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -11,51 +12,53 @@ include 'connect.php';
   <title>Appointment</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 </head>
+
 <body>
   <div class="container">
-  <button class="btn- btn-primary my-5"> <a href="user.php" class="text-light">Add User</a>
-  </button>
-  <table class="table">
-  <thead>
-    <tr>
-      <th scope="col">No.</th>
-      <th scope="col">Name</th>
-      <th scope="col">Email</th>
-      <th scope="col">Advisor</th>
-      <th scope="col">Department</th>
-      <th scope="col">operation</th>
-    </tr>
-  </thead>
-  <tbody>
+    <button class="btn- btn-primary my-5"> <a href="user.php" class="text-light">Add User</a>
+    </button>
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">No.</th>
+          <th scope="col">Name</th>
+          <th scope="col">Email</th>
+          <th scope="col">Advisor</th>
+          <th scope="col">Department</th>
+          <th scope="col">operation</th>
+        </tr>
+      </thead>
+      <tbody>
 
-  <?php
-  $sql="Select * from `users`";
-  $result=mysqli_query($con,$sql);
-  if($result){
-    while ( $row=mysqli_fetch_assoc($result)){
-      $id=$row['id'];
-      $name=$row['name'];
-      $email=$row['email'];
-      $advisor=$row['advisor'];
-      $department=$row['department'];
-      echo '<tr>
-      <th scope="row">'.$id.'</th>
-      <td>'.$name.'</td>
-      <td>'.$email.'</td>
-      <td>'.$advisor.'</td>
-      <td>'.$department.'</td>
+        <?php
+        $sql = "Select * from `users`";
+        $result = mysqli_query($con, $sql);
+        if ($result) {
+          while ($row = mysqli_fetch_assoc($result)) {
+            $id = $row['id'];
+            $name = $row['name'];
+            $email = $row['email'];
+            $advisor = $row['advisor'];
+            $department = $row['department'];
+            echo '<tr>
+      <th scope="row">' . $id . '</th>
+      <td>' . $name . '</td>
+      <td>' . $email . '</td>
+      <td>' . $advisor . '</td>
+      <td>' . $department . '</td>
       <td>
-    <button class="btn btn-primary"><a href="update.php" class="text-light" >Update</a></button>
-    <button class="btn btn-danger"><a href="delete.php? deleteid='.$id.' "class="text-light">Delete</a></button>
+    <button class="btn btn-primary"><a href="update.php? updateid=' . $id . '" class="text-light" >Update</a></button>
+    <button class="btn btn-danger"><a href="delete.php? deleteid=' . $id . ' "class="text-light">Delete</a></button>
     </td>
     </tr>';
-    }
-  }
-  ?>
-    
-    
-  </tbody>
-</table>
+          }
+        }
+        ?>
+
+
+      </tbody>
+    </table>
   </div>
 </body>
+
 </html>
