7a5f44d92f6f6901be87982ec7c7bce30473a6ac
pamran <ranjitpamas@gmail.com>
2021-07-07 11:13:18 -0500

    update new page
    click to homepage link added to the thank you page!

--- SSS/thankyou.php
+++ SSS/thankyou.php
@@ -22,6 +22,9 @@
 		<i class="fa fa-check main-content__checkmark" id="checkmark"></i>
 		<p class="main-content__body" data-lead-id="main-content-body">Thank You for making an Appointment! We look forward to see you soon.</p>
 	</div>
+	<p class="lead">
+    <a class="btn btn-primary btn-sm" href="testHomePage.html" role="button">Continue to homepage</a>
+  </p>
 
 	<footer class="site-footer" id="footer">
 		<p class="site-footer__fineprint" id="fineprint">Copyright ©2021| All Rights Reserved | ECU</p>
